5fa3d8470460a8f5878b27ad0e089ef28d141116
pm-osc <pm2.osc@gmail.com>
2025-09-27 07:11:29 +0200

    [~] trigger build
    Signed-off-by: pm-osc <pm2.osc@gmail.com>

--- janusgraph-core/src/main/java/org/janusgraph/graphdb/tinkerpop/optimize/step/HasStepFolder.java
+++ janusgraph-core/src/main/java/org/janusgraph/graphdb/tinkerpop/optimize/step/HasStepFolder.java
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// HELLO
+
 package org.janusgraph.graphdb.tinkerpop.optimize.step;
 
 import org.apache.tinkerpop.gremlin.process.traversal.Order;
