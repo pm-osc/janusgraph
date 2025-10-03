7f53b0557d06fa1796359eeca243a9550d8fbb24
pm-osc <pm2.osc@gmail.com>
2025-10-03 14:41:18 +0200

    [~] debug 9
    Signed-off-by: pm-osc <pm2.osc@gmail.com>

--- janusgraph-core/src/main/java/org/janusgraph/graphdb/tinkerpop/optimize/step/HasStepFolder.java
+++ janusgraph-core/src/main/java/org/janusgraph/graphdb/tinkerpop/optimize/step/HasStepFolder.java
@@ -47,6 +47,7 @@ import org.javatuples.Pair;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.Iterator;
 //import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
@@ -103,26 +104,30 @@ public interface HasStepFolder<S, E> extends Step<S, E> {
     }
     
     static boolean isIndexBackendsSupportsListPropertyOrdering(JanusGraphTransaction tx) {
-        return true;
-        // List<String> indexBackends = new ArrayList<String>();
-        // Iterator<String> configIt = tx.configuration().getKeys();
-        // while (configIt.hasNext()) {
-        //     String configKey = configIt.next();
-        //     if (configKey.matches("index\\.[^.]+\\.backend")) {
-        //         String configValue = tx.configuration().getString(configKey, "").toLowerCase().trim();
-        //         if (!configValue.isEmpty()) {
-        //             indexBackends.add(configValue);
-        //         }
+        List<String> indexBackends = new ArrayList<String>();
+        Iterator<String> configIt = tx.configuration().getKeys();
+        while (configIt.hasNext()) {
+            String configKey = configIt.next();
+            if (configKey.matches("index\\.[^.]+\\.backend")) {
+                String configValue = tx.configuration().getString(configKey);
+                if (configValue != null) {
+                    String finalConfigValue = configValue.toLowerCase().trim();
 
-        //         // get the value and check if it is lucene
-        //         // if any of the mixed index backends are Lucene, let's
-        //         // conclude immediately that the graph's mixed index backends
-        //         // do not support ordering list properties via mixed index
-        //         if (configValue.equals("lucene")) {
-        //             return false;
-        //         }
-        //     }
-        // }
+                    if (!finalConfigValue.isEmpty()) {
+                        indexBackends.add(configValue);
+                    }
+
+                    // get the value and check if it is lucene
+                    // if any of the mixed index backends are Lucene, let's
+                    // conclude immediately that the graph's mixed index backends
+                    // do not support ordering list properties via mixed index
+                    if (finalConfigValue.equals("lucene")) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
 
         // // if the graph does not have any mixed index backend configured,
         // // list property ordering via mixed index is surely not supported
